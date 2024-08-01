bbe5445ef3325c6067f6b9ff6816205d421a276c
Vincent Wilson <vincent.j.wilson317@gmail.com>
2024-07-31 18:46:45 -0700

    added app insights... maybe

--- api/BodyByBurgersInfoApi/Program.cs
+++ api/BodyByBurgersInfoApi/Program.cs
@@ -1,10 +1,12 @@
 using BodyByBurgersInfoApi.BusinessLogic;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var options = new ApplicationInsightsServiceOptions { ConnectionString = builder.Configuration["ApplicationInsights:ConnectionString"] };
+builder.Services.AddApplicationInsightsTelemetry(options: options);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +18,7 @@ builder.Services.AddScoped<IService<Review, ReviewDto>, ReviewService>();
 builder.Services.AddScoped<IService<Ingredient, IngredientDto>, IngredientService>();
 builder.Services.AddScoped<IService<Picture, PictureDto>, PictureService>();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 
 var app = builder.Build();
 app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
