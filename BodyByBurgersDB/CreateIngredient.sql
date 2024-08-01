3bc6e1cbaa732ff7f8e4e9a7edf609961bdecd8f
Vincent Wilson <vincent.j.wilson317@gmail.com>
2024-07-31 18:33:54 -0700

    stuff

--- BodyByBurgersDB/CreateIngredient.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE [dbo].[sp_CreateIngredient]
-  @name NVARCHAR(255),
-  @icon NVARCHAR(255),
-  @id INT OUTPUT
-AS
-  INSERT INTO [dbo].[Ingredient] ([Name], [Icon]) 
-  VALUES (@name, @icon)
-  SET @id = SCOPE_IDENTITY()
--- api/BodyByBurgersInfoApi/Entities/Ingredient.cs
+++ api/BodyByBurgersInfoApi/Entities/Ingredient.cs
@@ -3,5 +3,4 @@ public class Ingredient
     public int Id { get; set; }
     public string Name { get; set; }
     public string Icon { get; set; }
-    // public List<ReviewIngredient> ReviewIngredients { get; set; }
 }
