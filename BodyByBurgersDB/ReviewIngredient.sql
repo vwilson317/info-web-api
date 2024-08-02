ccbf1ecf246f7ae7e5439a2aaaee2934bf171df1
Vincent Wilson <vincent.j.wilson317@gmail.com>
2024-08-02 09:44:22 -0700

    changes to relationship table

--- BodyByBurgersDB/ReviewIngredient.sql
+++ BodyByBurgersDB/ReviewIngredient.sql
@@ -1,6 +1,8 @@
 CREATE TABLE [dbo].[ReviewIngredient]
 (
-    Id INT IDENTITY(1,1) PRIMARY KEY,
     ReviewId INT NOT NULL,
-    IngredientId INT NOT NULL
+    IngredientId INT NOT NULL,
+    CONSTRAINT PK_ReviewIngredient PRIMARY KEY (ReviewId, IngredientId),
+    CONSTRAINT FK_ReviewIngredient_Review FOREIGN KEY (ReviewId) REFERENCES Review(Id) ON DELETE CASCADE,
+    CONSTRAINT FK_ReviewIngredient_Ingredient FOREIGN KEY (IngredientId) REFERENCES Ingredient(Id) ON DELETE CASCADE
 )
